21a4066a1a16043fe4bbbbcc4b566a1161b25701
Patricia Roveri <patricia@pivot08.com.br>
2018-12-02 13:53:37 -0200

    htmls

---
+++ assets/js/utils.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+	
+	$("#toggle").click(function() {
+		$(this).toggleClass("active");
+		$(".sidebar").toggleClass("sidebar-active");
+	});
+	
+	function toggleIcon(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find(".more-less")
+        .toggleClass('glyphicon-plus glyphicon-minus');
+	}
+	$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+	$('.panel-group').on('shown.bs.collapse', toggleIcon);
+	
+	$('#carousel-hall').owlCarousel({
+		margin: 0,
+		autoplay: true,
+		nav: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			1440: {
+				items: 2
+			}
+			
+		}
+		
+	});
+	
+	$('#carousel-terreo').owlCarousel({
+		margin: 0,
+		autoplay: true,
+		nav: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			1500: {
+				items: 1
+			},
+			1600: {
+				items: 2
+			}
+			
+		}
+		
+	});
+	
+});
